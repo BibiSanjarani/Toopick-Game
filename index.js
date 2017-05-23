090eb60930cc88b46f49af5c20f706837fab310d
Bibi Sanjarani <bsanjarani@spartaglobal.co>
2017-05-23 14:16:54 +0100

    proto styling and structure

---
+++ index.js
@@ -0,0 +1,8 @@
+// get images to appear on the grid
+// toggle if you click on the image it turns over
+	//do if statement ; if match disable toggle
+			//add one to score 
+			// var score =0 
+			// score ++
+			
+	//, if not match toggle back.
